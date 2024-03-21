38121430d6d5b7765dafd1faf287495908d6a38b
aju513 <99ajumrzn@gmail.com>
2024-03-21 21:27:26 +0545

    lab17

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ using NccLabAju;
 
 class Program
 {
-    static void Main()
+    static async Task Main(string[] args)
     {
-        LincLambda temp = new LincLambda();
-        temp.linclambda();
+        await SayHelloAsync();
+    }
+
+    static async Task SayHelloAsync()
+    {
+        HttpClient client = new HttpClient();
+        string result = await client.GetStringAsync(
+            "https://fakerapi.it/api/v1/persons?_quantity=1&_gender=male&_birthday_start=2005-01-01"
+        );
+        Console.WriteLine(result);
     }
 }
---
+++ practical1/17/Async.cs
@@ -0,0 +1,21 @@
+// copy this whole in program.cs
+// using System;
+// using System.Collections.Generic;
+// using NccLabAju;
+
+// class Program
+// {
+//     static async Task Main(string[] args)
+//     {
+//         await SayHelloAsync();
+//     }
+
+//     static async Task SayHelloAsync()
+//     {
+//         HttpClient client = new HttpClient();
+//         string result = await client.GetStringAsync(
+//             "https://fakerapi.it/api/v1/persons?_quantity=1&_gender=male&_birthday_start=2005-01-01"
+//         );
+//         Console.WriteLine(result);
+//     }
+// }
